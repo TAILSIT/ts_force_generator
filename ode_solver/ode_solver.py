85fb0cd4b6054ad424aac276cdf02c21aab92ab4
Lars Kielhorn <lars.kielhorn@tailsit.com>
2024-09-23 13:26:41 +0200

    python script adjusted

--- ode_solver/ode_solver.py
+++ ode_solver/ode_solver.py
@@ -1,18 +1,7 @@
-from dataclasses import dataclass
 import numpy as np
-# from scipy.interpolate import interp1d
-
-@dataclass
-class ODESettings :
-    mass : float 
-    grav : float 
-    dt : float 
-    endt : float
-
-@dataclass
-class InitialConditions :
-    z0 : float = 0.
-    v0 : float = 0.
+from scipy.integrate import solve_ivp
+from functools import partial
+import argparse
 
 # IMPLICIT EULER
 # WE JUST SOLVE: m z'' = -m g + f(t,z)
@@ -24,71 +13,61 @@ class InitialConditions :
 #
 #  ---> A x' = M x + h
 #
-def ode_eb( settings : ODESettings, ic : InitialConditions, func ):
-    m = settings.mass
-    dt = settings.dt
-    A = np.array( [[1.0, 0.0], [0.0,   m]])
-    M = np.array( [[0.0, 1.0], [0.0, 0.0]])
-    AM = A/dt - M
-    iAM = np.linalg.inv( AM )
-    mg = m * settings.grav
+def ode_system( t, y, m, g, f):
+    z, v = y
+    dzdt = v
+    dvdt = -g + f(t,z)/m
+    return [dzdt, dvdt]
 
-    t = np.arange( 0, settings.endt, dt)
-    z = np.zeros_like(t)
-    v = np.zeros_like(t)
-    a = np.zeros_like(t)
+def export_to_file( filename: str, t, z, v, f ):
+    header = "t z v f=m z''"
+    data = np.column_stack((t,z,v,f))
+    np.savetxt( filename, data, header=header, fmt='%6e')
 
-    # initial conditions
-    z[0] = ic.z0
-    v[0] = ic.v0
-    a[0] = -settings.grav + func( t[0] )/settings.mass
-
-    # time-stepping loop
-    for i in range(1,len(t)):
-        xim1 = np.array( [ z[i-1], v[i-1]])
-        fi = func(t[i])
-        h = np.array( [0, -mg + fi])
-        rhs = np.dot( M, xim1) + h
-        k = np.dot( iAM, rhs )
-        xi = xim1 + k
-        z[i] = xi[0]
-        v[i] = xi[1]
-        a[i] = -settings.grav + fi / m
-
-    return t, z, v, a
-
-def export_to_file( filename: str, t, z, v, a ):
-    data = np.column_stack((t,z,v,a))
-    np.savetxt( filename, data, fmt='%6e')
-
-def load_forces( filename ):
-    data = np.loadtxt( filename, dtype=float, comments='#')
-    t = data[:,0]
-    f = data[:,3]
-    return t, f
-
-def force( t : float ) -> float:
-    if t <= .06 or t >= 0.25: 
-        return 0.0
-    else:
-        return 9.81 * 6e-3
+def force( t: float, z: float, scale:float ) -> float:
+    zz = np.abs(z)
+    args = np.array( [20.0-1000.0*zz, 25.0-1000.0*zz, 85.0-1000.0*zz])
+    exps = np.exp( args )
+    eval = 2.0/(1.0+exps[0]) - 1.0/(1.0+exps[1]) - 1.0/(1.0+exps[2])
+    return eval * scale
 
 if __name__ == "__main__":
-
+    # 
+    # argument parser 
+    parser = argparse.ArgumentParser(description="simple ode solver / illustrate force of a falling magnet")
+    parser.add_argument( "ode_method", type=str, help="The ode method") # e.g. 'RK45' or 'BDF'
+    parser.add_argument( "dt", type=float, help="The time step size")
+    #
+    # parse arguments
+    args = parser.parse_args()
     #
     # COMPUTE MASS BY MEANS OF A CYLINDER AND GIVEN DENSITY
     r_cyl = 6.35e-3
     h_cyl = 6.35e-3
     rho_cyl = 7459.0
     mass = np.pi * r_cyl**2 * h_cyl * rho_cyl
-    settings = ODESettings(mass=mass,grav=9.81,dt=5e-4,endt=3e-1)
 
-    ic = InitialConditions(z0=0.0,v0=0.0)
+    y0 = [0.0,0.0]
+    
+    t_span = (0,3e-1)
+    dt = args.dt
 
-    # tin, fin = load_forces( 'implicit_dt5e-3.dat')
-    # fh = interp1d( tin, fin, kind='linear' )
+    grav = 9.81
+    mg = mass * grav
+    fz = partial( force, scale=mg )
+    time_steps = np.arange(0,t_span[1],dt)
     
-    t, z, v, a = ode_eb( settings=settings, ic=ic, func=force )
+    sol = solve_ivp( ode_system, t_span, y0, args=(mass, grav, fz), t_eval=time_steps, method=args.ode_method)
+
+    t = sol.t
+    z = sol.y[0]
+    v = sol.y[1]
+
+    f = np.zeros_like(t)
+
+    for i, ti in enumerate(t):
+        f[i] = -mg + fz(ti,z[i])
     
-    export_to_file( 'ode_eb.dat', t, z, v, a)
+    fname = f"ode_{args.ode_method}_dt{args.dt:.0e}.dat"
+    export_to_file( fname, t, z, v, f )
     
